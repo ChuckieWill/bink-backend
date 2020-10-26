20968aa94645a5e3fd5bf42e93fb949b3329fd18
ChuckieWill <1747363320@qq.com>
2020-10-26 22:44:50 +0800

    添加注册登录模块及用户验证

--- config/config.js
+++ config/config.js
@@ -10,6 +10,16 @@ module.exports = {
     password: '123456',//数据库用户密码
     host: 'localhost',//数据库主机名 
     port:3306,//数据库端口
-
+  },
+  //令牌配置信息
+  security:{
+    secretKey:"abcdefg",//需要特别长且无规律，一旦拿到该密码，则可以破解jsonwebtoken的所有令牌
+    expiresIn:60*60*24*30,//令牌过期时间，单位：秒
+  },
+  //wx登录验证配置信息
+  wx:{
+    appId: '',//小程序 appId
+    appSecret: '',//小程序 appSecret
+    loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
   }
 }
---
+++ middlewares/auth.js
@@ -0,0 +1,48 @@
+//令牌验证
+const basicAuth = require('basic-auth')//解析HpptBasicAuth方式传递的令牌
+const jwt = require('jsonwebtoken')//令牌处理库
+class Auth {
+  //实例化类时传入具体API的访问级别（可访问的角色）
+  constructor(level){
+    this.level = level || 1//设置实例化的api权限要求
+    //类变量-scope角色定义
+    Auth.USER = 8 //普通用户
+    Auth.ADMIN = 16 //管理员
+    Auth.SUPER_ADMIN = 32 //超级管理员
+  }
+  //属性
+  get m(){
+    return async (ctx, next) => {
+      //获取前端传来的token
+      const userToken = basicAuth(ctx.req)
+      //token不存在则阻止访问
+      if(!userToken || !userToken.name){
+        throw new global.errs.Forbbiden('未传token')
+      }
+      try {
+        //验证token是否合法
+        var decode = jwt.verify(userToken.name, global.config.security.secretKey)
+      } catch (error) {
+        if(error.name == 'TokenExpiredError'){
+          throw new global.errs.Forbbiden('token已过期')
+        }
+        throw new global.errs.Forbbiden('token不合法')
+      }
+      //权限判断（用户角色判断），
+      if(decode.scope < this.level){
+        throw new global.errs.Forbbiden('权限不够')
+      }
+      //将保存在jwt中的用户id:uid和scop取出
+      //并保存在上下文auth中，便于随时使用
+      ctx.auth = {
+        uid: decode.uid,
+        scope: decode.scope
+      }
+      await next()
+    }
+  }
+}
+
+module.exports = {
+  Auth
+}
